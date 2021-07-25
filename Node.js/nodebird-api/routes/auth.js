0ededc03fa1d252f60bc42e10d9e543c225e71bd
gxdxx <nkd0310@naver.com>
2021-07-25 10:03:34 +0900

    feat: API 서버, 호출 서버, CORS 구현

--- Node.js/nodebird-api/routes/auth.js
+++ Node.js/nodebird-api/routes/auth.js
@@ -26,7 +26,7 @@ router.post('/join', isNotLoggedIn, async(req, res, next) => {
 });
 
 router.post('/login', isNotLoggedIn, (req, res, next) => {
-    passport.authenticate('local', (authError, user, info) => {
+    passport.authenticate('local', { session: false }, (authError, user, info) => {
         if (authError) {
             console.error(authError);
             return next(authError);
--- Node.js/nodebird-api/routes/v2.js
+++ Node.js/nodebird-api/routes/v2.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const url = require('url');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
+router.use((req, res, next) => {
+    const domain = await Domain.findOne({
+        where: { host: url.parse(req.get('origin')) ? host : undefined }
+    });
+    if (domain) {
+        cors({
+            origin: true,
+            credentials: true,
+        })(req, res, next);
+    } else {
+        next();
+    }
+});
 
-router.use(deprecated);
-
-router.post('/token', async(req, res) => {
+router.post('/token', apiLimiter, async(req, res) => {
     const { clientSecret } = req.body;
     try {
         const domain = await Domain.findOne({
@@ -28,9 +41,10 @@ router.post('/token', async(req, res) => {
             id: domain.User.id,
             nick: domain.User.nick,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m', // 1분
+            expiresIn: '30m', // 30분
             issuer: 'nodebird',
         });
+        res.setHeader('Access-Control-Allow-Origin', '*');
         return res.json({
             code: 200,
             message: '토큰이 발급되었습니다',
@@ -45,13 +59,14 @@ router.post('/token', async(req, res) => {
     }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
     res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
     Post.findAll({ where: { userId: req.decoded.id } })
         .then((posts) => {
+            console.log(posts);
             res.json({
                 code: 200,
                 payload: posts,
@@ -66,13 +81,13 @@ router.get('/posts/my', verifyToken, (req, res) => {
         });
 });
 
-router.get('/posts/hashtag/:title', verifyToken, async(req, res) => {
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async(req, res) => {
     try {
         const hashtag = await Hashtag.findOne({ where: { title: req.params.title } });
         if (!hashtag) {
             return res.status(404).json({
                 code: 404,
-                messgae: '검색 결과가 없습니다.',
+                message: '검색 결과가 없습니다',
             });
         }
         const posts = await hashtag.getPosts();
