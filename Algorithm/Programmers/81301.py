c696f9dce5bb5712f893606794512160e305232e
gxdxx <nkd0310@naver.com>
2021-11-23 00:04:53 +0900

    add: 숫자 문자열과 영단어

---
+++ Algorithm/Programmers/81301.py
@@ -0,0 +1,8 @@
+def solution(s):
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i in range(len(numbers)):
+        s = s.replace(numbers[i], str(i))
+    
+    answer = int(s)
+    return answer
